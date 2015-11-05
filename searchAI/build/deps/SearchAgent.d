bafa12b6f0e7067fc4801ceda21f8d6f94bea63e
rahul <pracsys@margaery.cs.rutgers.edu>
2015-11-04 23:04:58 -0500

    A Star and Search Agent files

---
+++ searchAI/build/deps/SearchAgent.d
@@ -0,0 +1,288 @@
+objs/SearchAgent.o: ../src/SearchAgent.cpp /usr/include/stdc-predef.h \
+ ../../steerlib/include/SteerLib.h ../../steerlib/include/Globals.h \
+ ../../steerlib/include/Util.h ../../steerlib/include/util/Curve.h \
+ /usr/include/c++/4.8/iostream \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/os_defines.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/cpu_defines.h \
+ /usr/include/c++/4.8/ostream /usr/include/c++/4.8/ios \
+ /usr/include/c++/4.8/iosfwd /usr/include/c++/4.8/bits/stringfwd.h \
+ /usr/include/c++/4.8/bits/memoryfwd.h \
+ /usr/include/c++/4.8/bits/postypes.h /usr/include/c++/4.8/cwchar \
+ /usr/include/wchar.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stddef.h \
+ /usr/include/xlocale.h /usr/include/x86_64-linux-gnu/bits/wchar2.h \
+ /usr/include/c++/4.8/exception \
+ /usr/include/c++/4.8/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/4.8/bits/exception_ptr.h \
+ /usr/include/c++/4.8/bits/exception_defines.h \
+ /usr/include/c++/4.8/bits/nested_exception.h \
+ /usr/include/c++/4.8/bits/char_traits.h \
+ /usr/include/c++/4.8/bits/stl_algobase.h \
+ /usr/include/c++/4.8/bits/functexcept.h \
+ /usr/include/c++/4.8/bits/cpp_type_traits.h \
+ /usr/include/c++/4.8/ext/type_traits.h \
+ /usr/include/c++/4.8/ext/numeric_traits.h \
+ /usr/include/c++/4.8/bits/stl_pair.h /usr/include/c++/4.8/bits/move.h \
+ /usr/include/c++/4.8/bits/concept_check.h \
+ /usr/include/c++/4.8/type_traits \
+ /usr/include/c++/4.8/bits/stl_iterator_base_types.h \
+ /usr/include/c++/4.8/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/4.8/debug/debug.h \
+ /usr/include/c++/4.8/bits/stl_iterator.h /usr/include/c++/4.8/cstdint \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdint.h /usr/include/stdint.h \
+ /usr/include/c++/4.8/bits/localefwd.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/c++locale.h \
+ /usr/include/c++/4.8/clocale /usr/include/locale.h \
+ /usr/include/x86_64-linux-gnu/bits/locale.h /usr/include/c++/4.8/cctype \
+ /usr/include/ctype.h /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/c++/4.8/bits/ios_base.h \
+ /usr/include/c++/4.8/ext/atomicity.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/gthr.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/timex.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/atomic_word.h \
+ /usr/include/c++/4.8/bits/locale_classes.h /usr/include/c++/4.8/string \
+ /usr/include/c++/4.8/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/c++allocator.h \
+ /usr/include/c++/4.8/ext/new_allocator.h /usr/include/c++/4.8/new \
+ /usr/include/c++/4.8/bits/ostream_insert.h \
+ /usr/include/c++/4.8/bits/cxxabi_forced.h \
+ /usr/include/c++/4.8/bits/stl_function.h \
+ /usr/include/c++/4.8/backward/binders.h \
+ /usr/include/c++/4.8/bits/range_access.h \
+ /usr/include/c++/4.8/bits/basic_string.h \
+ /usr/include/c++/4.8/initializer_list \
+ /usr/include/c++/4.8/ext/string_conversions.h \
+ /usr/include/c++/4.8/cstdlib /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib.h /usr/include/c++/4.8/cstdio \
+ /usr/include/libio.h /usr/include/_G_config.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h /usr/include/c++/4.8/cerrno \
+ /usr/include/errno.h /usr/include/x86_64-linux-gnu/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/x86_64-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/c++/4.8/bits/functional_hash.h \
+ /usr/include/c++/4.8/bits/hash_bytes.h \
+ /usr/include/c++/4.8/bits/basic_string.tcc \
+ /usr/include/c++/4.8/bits/locale_classes.tcc \
+ /usr/include/c++/4.8/streambuf /usr/include/c++/4.8/bits/streambuf.tcc \
+ /usr/include/c++/4.8/bits/basic_ios.h \
+ /usr/include/c++/4.8/bits/locale_facets.h /usr/include/c++/4.8/cwctype \
+ /usr/include/wctype.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/ctype_base.h \
+ /usr/include/c++/4.8/bits/streambuf_iterator.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/ctype_inline.h \
+ /usr/include/c++/4.8/bits/locale_facets.tcc \
+ /usr/include/c++/4.8/bits/basic_ios.tcc \
+ /usr/include/c++/4.8/bits/ostream.tcc /usr/include/c++/4.8/istream \
+ /usr/include/c++/4.8/bits/istream.tcc /usr/include/c++/4.8/vector \
+ /usr/include/c++/4.8/bits/stl_construct.h \
+ /usr/include/c++/4.8/ext/alloc_traits.h \
+ /usr/include/c++/4.8/bits/alloc_traits.h \
+ /usr/include/c++/4.8/bits/ptr_traits.h \
+ /usr/include/c++/4.8/bits/stl_uninitialized.h \
+ /usr/include/c++/4.8/bits/stl_vector.h \
+ /usr/include/c++/4.8/bits/stl_bvector.h \
+ /usr/include/c++/4.8/bits/vector.tcc \
+ ../../steerlib/include/util/Geometry.h /usr/include/assert.h \
+ /usr/include/math.h /usr/include/x86_64-linux-gnu/bits/huge_val.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_valf.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_vall.h \
+ /usr/include/x86_64-linux-gnu/bits/inf.h \
+ /usr/include/x86_64-linux-gnu/bits/nan.h \
+ /usr/include/x86_64-linux-gnu/bits/mathdef.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathinline.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/float.h \
+ ../../steerlib/include/Globals.h ../../steerlib/include/util/Color.h \
+ ../../steerlib/include/util/Color.h \
+ ../../steerlib/include/util/CommandLineParser.h \
+ /usr/include/c++/4.8/sstream /usr/include/c++/4.8/bits/sstream.tcc \
+ /usr/include/c++/4.8/map /usr/include/c++/4.8/bits/stl_tree.h \
+ /usr/include/c++/4.8/bits/stl_map.h /usr/include/c++/4.8/tuple \
+ /usr/include/c++/4.8/utility /usr/include/c++/4.8/bits/stl_relops.h \
+ /usr/include/c++/4.8/array /usr/include/c++/4.8/stdexcept \
+ /usr/include/c++/4.8/bits/uses_allocator.h \
+ /usr/include/c++/4.8/bits/stl_multimap.h \
+ ../../steerlib/include/util/DrawLib.h /usr/include/GL/glew.h \
+ /usr/include/GL/glu.h /usr/include/GL/gl.h /usr/include/GL/glut.h \
+ /usr/include/GL/freeglut_std.h \
+ ../../steerlib/include/util/DynamicLibrary.h \
+ ../../steerlib/include/util/GenericException.h \
+ ../../steerlib/include/util/Geometry.h \
+ ../../steerlib/include/util/HighResCounter.h \
+ ../../steerlib/include/util/MemoryMapper.h \
+ ../../steerlib/include/util/Misc.h /usr/include/c++/4.8/fstream \
+ /usr/include/c++/4.8/bits/codecvt.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/basic_file.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/c++io.h \
+ /usr/include/c++/4.8/bits/fstream.tcc /usr/include/c++/4.8/set \
+ /usr/include/c++/4.8/bits/stl_set.h \
+ /usr/include/c++/4.8/bits/stl_multiset.h \
+ ../../steerlib/include/util/XMLParser.h \
+ ../../steerlib/include/util/XMLParserPrivate.h \
+ ../../external/tinyxml/ticpp.h ../../external/tinyxml/tinyxml.h \
+ /usr/include/string.h /usr/include/x86_64-linux-gnu/bits/string3.h \
+ ../../external/tinyxml/ticpprc.h /usr/include/c++/4.8/memory \
+ /usr/include/c++/4.8/bits/stl_tempbuf.h \
+ /usr/include/c++/4.8/bits/stl_raw_storage_iter.h \
+ /usr/include/c++/4.8/typeinfo /usr/include/c++/4.8/ext/concurrence.h \
+ /usr/include/c++/4.8/functional /usr/include/c++/4.8/bits/unique_ptr.h \
+ /usr/include/c++/4.8/bits/shared_ptr.h \
+ /usr/include/c++/4.8/bits/shared_ptr_base.h \
+ /usr/include/c++/4.8/backward/auto_ptr.h \
+ ../../steerlib/include/util/Mutex.h \
+ ../../steerlib/include/util/PerformanceProfiler.h \
+ ../../steerlib/include/util/HighResCounter.h \
+ ../../steerlib/include/util/StateMachine.h \
+ ../../steerlib/include/util/GenericException.h \
+ ../../steerlib/include/util/Misc.h \
+ ../../steerlib/include/util/ThreadedTaskManager.h \
+ /usr/include/c++/4.8/queue /usr/include/c++/4.8/deque \
+ /usr/include/c++/4.8/bits/stl_deque.h \
+ /usr/include/c++/4.8/bits/deque.tcc /usr/include/c++/4.8/bits/stl_heap.h \
+ /usr/include/c++/4.8/bits/stl_queue.h \
+ ../../steerlib/include/util/XMLParser.h \
+ ../../steerlib/include/recfileio/RecFileIO.h \
+ ../../steerlib/include/recfileio/RecFileIOPrivate.h \
+ ../../steerlib/include/util/MemoryMapper.h \
+ ../../steerlib/include/testcaseio/TestCaseIO.h \
+ ../../steerlib/include/testcaseio/TestCaseIOPrivate.h \
+ ../../steerlib/include/simulation/Camera.h \
+ ../../steerlib/include/util/Curve.h \
+ ../../steerlib/include/griddatabase/GridDatabase2D.h \
+ /usr/include/c++/4.8/stack /usr/include/c++/4.8/bits/stl_stack.h \
+ ../../steerlib/include/griddatabase/GridDatabase2DPrivate.h \
+ ../../steerlib/include/util/Mutex.h \
+ ../../steerlib/include/griddatabase/GridCell.h \
+ ../../steerlib/include/interfaces/SpatialDatabaseItem.h \
+ ../../steerlib/include/testcaseio/AgentInitialConditions.h \
+ ../../steerlib/include/testcaseio/Behaviour.h \
+ ../../steerlib/include/testcaseio/BehaviourParameter.h \
+ ../../steerlib/include/testcaseio/ObstacleInitialConditions.h \
+ ../../steerlib/include/obstacles/BoxObstacle.h \
+ ../../steerlib/include/interfaces/ObstacleInterface.h \
+ ../../steerlib/include/obstacles/CircleObstacle.h \
+ ../../steerlib/include/obstacles/OrientedBoxObstacle.h \
+ ../../steerlib/include/obstacles/OrientedWallObstacle.h \
+ ../../steerlib/include/obstacles/PolygonObstacle.h \
+ ../../external/mersenne/MersenneTwister.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ ../../steerlib/include/interfaces/EngineInterface.h \
+ ../../steerlib/include/recfileio/RecFileIO.h \
+ ../../steerlib/include/interfaces/AgentInterface.h \
+ ../../steerlib/include/interfaces/EngineControllerInterface.h \
+ ../../steerlib/include/util/DrawLib.h \
+ ../../steerlib/include/util/DynamicLibrary.h \
+ ../../steerlib/include/simulation/Clock.h /usr/include/c++/4.8/algorithm \
+ /usr/include/c++/4.8/bits/stl_algo.h \
+ /usr/include/c++/4.8/bits/algorithmfwd.h /usr/include/c++/4.8/random \
+ /usr/include/c++/4.8/cmath /usr/include/c++/4.8/limits \
+ /usr/include/c++/4.8/bits/random.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/opt_random.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/x86intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/ia32intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/mm_malloc.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/immintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/fxsrintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/adxintrin.h \
+ /usr/include/c++/4.8/bits/random.tcc /usr/include/c++/4.8/numeric \
+ /usr/include/c++/4.8/bits/stl_numeric.h \
+ ../../steerlib/include/simulation/SimulationOptions.h \
+ ../../steerlib/include/interfaces/ModuleInterface.h \
+ ../../util/include/LogData.h ../../util/include/Logger.h \
+ ../../util/include/LogObject.h /usr/include/c++/4.8/cstring \
+ ../../util/include/UtilGlobals.h \
+ ../../steerlib/include/testcaseio/AgentInitialConditions.h \
+ ../../steerlib/include/testcaseio/ObstacleInitialConditions.h \
+ ../../steerlib/include/griddatabase/GridCell.h \
+ ../../steerlib/include/griddatabase/GridDatabase2D.h \
+ ../../steerlib/include/griddatabase/GridDatabasePlanningDomain.h \
+ ../../steerlib/include/planning/BestFirstSearchPlanner.h \
+ ../../steerlib/include/obstacles/BoxObstacle.h \
+ ../../steerlib/include/obstacles/OrientedBoxObstacle.h \
+ ../../steerlib/include/obstacles/CircleObstacle.h \
+ ../../steerlib/include/planning/BestFirstSearchPlanner.h \
+ ../../steerlib/include/simulation/Camera.h \
+ ../../steerlib/include/simulation/Clock.h \
+ ../../steerlib/include/simulation/SimulationOptions.h \
+ ../../steerlib/include/simulation/SimulationEngine.h \
+ ../../steerlib/include/util/StateMachine.h \
+ ../../steerlib/include/simulation/SteeringCommand.h \
+ ../../steerlib/include/benchmarking/AgentMetricsCollector.h \
+ ../../steerlib/include/benchmarking/MetricsData.h \
+ ../../steerlib/include/benchmarking/MetricsData.h \
+ ../../steerlib/include/benchmarking/SimulationMetricsCollector.h \
+ ../../steerlib/include/benchmarking/AgentMetricsCollector.h \
+ ../../steerlib/include/benchmarking/BenchmarkEngine.h \
+ ../../steerlib/include/interfaces/BenchmarkTechniqueInterface.h \
+ ../../steerlib/include/benchmarking/SimulationMetricsCollector.h \
+ ../../steerlib/include/benchmarking/BenchmarkEnginePrivate.h \
+ ../../steerlib/include/benchmarking/CompositeTechnique01.h \
+ ../../steerlib/include/benchmarking/CompositeTechnique02.h \
+ ../../steerlib/include/benchmarking/EnergyEfficiencyTechnique.h \
+ ../../steerlib/include/benchmarking/AccelerationEfficiencyTechnique.h \
+ ../../steerlib/include/benchmarking/CompositeTechniquePLE.h \
+ ../../steerlib/include/benchmarking/CompositeTechniqueGraph.h \
+ ../../steerlib/include/benchmarking/CompositeTechnique02.h \
+ ../../steerlib/include/benchmarking/CompositeTechnique01.h \
+ ../../steerlib/include/benchmarking/CompositeTechnique02.h \
+ ../../steerlib/include/benchmarking/EnergyEfficiencyTechnique.h \
+ ../../steerlib/include/benchmarking/AccelerationEfficiencyTechnique.h \
+ ../../steerlib/include/interfaces/AgentInterface.h \
+ ../../steerlib/include/interfaces/BenchmarkTechniqueInterface.h \
+ ../../steerlib/include/interfaces/EngineControllerInterface.h \
+ ../../steerlib/include/interfaces/EngineInterface.h \
+ ../../steerlib/include/interfaces/ObstacleInterface.h \
+ ../../steerlib/include/interfaces/ModuleInterface.h \
+ ../../steerlib/include/interfaces/SpatialDatabaseItem.h \
+ ../../steerlib/include/modules/DummyAIModule.h \
+ ../../steerlib/include/modules/MetricsCollectorModule.h \
+ ../../steerlib/include/modules/RecFilePlayerModule.h \
+ ../../steerlib/include/modules/SimulationRecorderModule.h \
+ ../../steerlib/include/modules/SteerBenchModule.h \
+ ../../steerlib/include/benchmarking/BenchmarkEngine.h \
+ ../../steerlib/include/modules/MetricsCollectorModule.h \
+ ../../steerlib/include/modules/SteerBugModule.h \
+ ../../steerlib/include/modules/TestCasePlayerModule.h \
+ ../include/SearchAgent.h ../include/SearchAIModule.h \
+ ../../util/include/Logger.h \
+ ../../steerlib/include/planning/AStarPlanner.h \
+ ../include/SearchAIModule.h
